8ff372e87ec297c9b433f5733ceca94b45fe60c9
Achrefothmeni <othmeniachref@gmail.com>
2020-03-21 15:43:38 +0100

    first commit

---
+++ src/debo/DBConnection.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package debo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author dell
+ */
+public class DBConnection {
+    
+    private static DBConnection instance;
+    private Connection connexion;
+    private String url = "JDBC:mysql://localhost:3306/debo_db";
+    private String user = "root";
+    private String password = "";
+
+    private DBConnection() {
+        try {
+            connexion = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            System.out.println("Probleme de connexion");
+        }
+    }
+
+    public static DBConnection getInstance() {
+        if (DBConnection.instance == null) {
+            DBConnection.instance = new DBConnection();
+        }
+        return DBConnection.instance;
+    }
+
+    public Connection getConnexion() {
+        return this.connexion;
+    }
+
+    
+}
