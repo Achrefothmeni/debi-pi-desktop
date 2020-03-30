a1fae54d8f36c8007f7655b8f65af2afef4f5e60
Achrefothmeni <othmeniachref@gmail.com>
2020-03-30 14:25:40 +0100

    added findOne, findAll, delete to UserDAO

--- src/user/module/dao/UserDAO.java
+++ src/user/module/dao/UserDAO.java
@@ -8,8 +8,10 @@ package user.module.dao;
 import debo.DBConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import user.module.model.User;
@@ -62,7 +64,67 @@ public class UserDAO {
         } catch (SQLException ex) {
             System.err.println(ex.getMessage());
         }
-
-        
     }
+    
+    public User findOne(String username){
+        String query = "SELECT * FROM fos_user WHERE username = ?";
+        PreparedStatement preparedStmt;
+        User user = new User();
+        try{
+            preparedStmt = connection.prepareStatement(query);
+            preparedStmt.setString(1, username);
+            ResultSet rs = preparedStmt.executeQuery();
+            while ( rs.next() ){
+                user.setUsername(rs.getString("username"));
+                user.setEmail(rs.getString("email"));
+                user.setNom(rs.getString("nom"));
+                user.setPrenom(rs.getString("prenom"));
+                user.setMatFiscal(rs.getString("mat_fiscal"));
+                user.setStatus(rs.getString("status_livreur"));
+                user.setNumTel(rs.getInt("numero_tel"));
+                user.setRoles(rs.getString("roles"));
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return user;
+    }
+    
+    public ArrayList<User> findAll(String username){
+        ArrayList<User> users = new ArrayList(); 
+        String query = "SELECT * FROM fos_user";
+        PreparedStatement preparedStmt;
+        try{
+            preparedStmt = connection.prepareStatement(query);
+            ResultSet rs = preparedStmt.executeQuery();
+            while ( rs.next() ){
+                User user = new User();
+                user.setUsername(rs.getString("username"));
+                user.setEmail(rs.getString("email"));
+                user.setNom(rs.getString("nom"));
+                user.setPrenom(rs.getString("prenom"));
+                user.setMatFiscal(rs.getString("mat_fiscal"));
+                user.setStatus(rs.getString("status_livreur"));
+                user.setNumTel(rs.getInt("numero_tel"));
+                user.setRoles(rs.getString("roles"));
+                users.add(user);
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return users;
+    }
+    
+    public void delete(User user){
+        String query = "DELETE FROM fos_user WHERE username=?";
+        PreparedStatement preparedStmt;
+        try{
+            preparedStmt = connection.prepareStatement(query);
+            preparedStmt.setString(1, user.getUsername());
+            preparedStmt.execute();
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
 }
