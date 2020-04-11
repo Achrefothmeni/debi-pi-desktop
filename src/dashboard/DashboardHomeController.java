a6006ca51e8dec38e0949a0b2942c87ac8dddbf9
Achrefothmeni <othmeniachref@gmail.com>
2020-04-11 18:05:38 +0100

    update dashboard

--- src/dashboard/DashboardHomeController.java
+++ src/dashboard/DashboardHomeController.java
@@ -6,8 +6,13 @@
 package dashboard;
 
 import debo.Authentication;
+import debo.DBConnection;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,9 +23,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import user.module.User;
 import user.module.UserLoginController;
 
 /**
@@ -32,14 +42,58 @@ public class DashboardHomeController implements Initializable {
 
     @FXML
     private Text username;
+    @FXML
+    private Text nbCmdText;
+    @FXML
+    private Text nbUsersText;
+    @FXML
+    private Text nbLivText;
+    @FXML
+    private BarChart<?, ?> barChart;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        Connection connection = DBConnection.getInstance().getConnexion();
         username.setText(Authentication.authenticatedUser.getUsername());
+        String query = "SELECT COUNT(*) FROM fos_user";
+        PreparedStatement preparedStmt;
+        User user = new User();
+        try{
+            preparedStmt = connection.prepareStatement(query);
+            ResultSet rs = preparedStmt.executeQuery();
+            while ( rs.next() ){
+                nbUsersText.setText(String.valueOf(rs.getInt(1))+" utilisateurs");
+            }
+            query = "SELECT COUNT(*) FROM commande";
+            preparedStmt = connection.prepareStatement(query);
+            preparedStmt.executeQuery();
+            rs = preparedStmt.executeQuery();
+            while ( rs.next() ){
+                nbCmdText.setText(String.valueOf(rs.getInt(1))+" commandes");
+            }
+            query = "SELECT COUNT(*) FROM livraison";
+            preparedStmt = connection.prepareStatement(query);
+            preparedStmt.executeQuery();
+            rs = preparedStmt.executeQuery();
+            while ( rs.next() ){
+                nbLivText.setText(String.valueOf(rs.getInt(1))+" livraisons");
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
         
+        XYChart.Series dataSeries1 = new XYChart.Series();
+        dataSeries1.setName("Article");
+        
+        dataSeries1.getData().add(new XYChart.Data("Desktop", 567));
+        dataSeries1.getData().add(new XYChart.Data("Phone"  , 65));
+        dataSeries1.getData().add(new XYChart.Data("Tablet"  , 23));
+        
+         barChart.getData().add(dataSeries1);
+         
     }    
 
     @FXML
